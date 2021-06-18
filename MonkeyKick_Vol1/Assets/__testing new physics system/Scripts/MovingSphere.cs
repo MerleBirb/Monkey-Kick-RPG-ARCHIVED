b3218487df08b658f8d0dd48ad6de85ab04e933b
MerleBirb <xylan420@gmail.com>
2021-06-18 15:38:02 -0500

    trying out new physics

---
+++ MonkeyKick_Vol1/Assets/__testing new physics system/Scripts/MovingSphere.cs
@@ -0,0 +1,23 @@
+//===== MOVING SPHERE =====//
+/*
+6/18/21
+Description:
+- tutorial stuff
+
+Author: Merlebirb, thanks to Catlike Coding
+*/
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MonkeyKick
+{
+    public class MovingSphere : MonoBehaviour
+    {
+        private PlayerControls input;
+        private void Update()
+        {
+            
+        }
+    }
+}
