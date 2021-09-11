2613cc202e66d169eac68ae0e067a594a849c6c9
Merle Roji <xylan420@gmail.com>
2021-09-11 04:46:15 -0400

    installed sabrecsg for building maps

--- MonkeyKick_Vol1/Assets/_MK_Scripts/Camera/CameraControls.cs
+++ MonkeyKick_Vol1/Assets/_MK_Scripts/Camera/CameraControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/_GAME/Camera/CameraControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/_MK_Scripts/Camera/CameraControls.inputactions'
 
 using System;
 using System.Collections;
